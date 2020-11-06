215cd0cb5a659a9545f233e786839601f7c4884e
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-11-06 11:48:37 +0000

    Update erro.php, footer.php, and 4 more files...

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<header>
+    <title>Rc</title>
+</header>
+<html>
+<body>
+    <?php
+    include('header.php');
+    ?>
+    <br>Aqui fica o restande conteudo da pagina.<br>
+    Vamos la fazer uma soma, só de exemplo,
+    <?php
+    echo 3+5;
+    ?>
+    <br>
+    <?php
+    include('footer.php');
+    ?> 
+    <?php
+    include('login.php');
+?>
+    </body>
+</html>
+        
---
+++ main.php
@@ -0,0 +1,4 @@
+<?php
+include("header.php");
+echo "Aqui ficara a parte do <B>conteudo</B> (esta sera a parte variavel do nosso site)<br>";
+include("footer.php");
